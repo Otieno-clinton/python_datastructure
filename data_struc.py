d938d1953cd8994f4fb2dbe2b53065796b615b84
Clinton Oluoch <otienoclinton2022@gmail.com>
2024-09-28 18:02:39 +0300

    Add initial Python file

---
+++ data_struc.py
@@ -0,0 +1,14 @@
+# Step 1: Create an empty list
+my_list = []
+
+# Step 2: Append the elements 10, 20, 30, 40
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# Step 3: Insert the value 15 at the second position
+my_list.insert(1, 15)
+
+# Step 4: Extend my_list with [50, 60, 70]
+my_list.extend([50, 60, 70])
